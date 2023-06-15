1295af430f87d4f017ccb76004e9cb72b07213d9
vjlion <89218741998@mail.ru>
2023-06-15 12:55:34 +0300

    bi

---
+++ Week3/Project/PySparkJob.py
@@ -0,0 +1,30 @@
+import io
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, datediff
+from pyspark.sql import functions as F
+
+
+def process(spark, input_file, target_path):
+    # TODO Ваш код
+
+
+def main(argv):
+    input_path = argv[0]
+    print("Input path to file: " + input_path)
+    target_path = argv[1]
+    print("Target path: " + target_path)
+    spark = _spark_session()
+    process(spark, input_path, target_path)
+
+
+def _spark_session():
+    return SparkSession.builder.appName('PySparkJob').getOrCreate()
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1:]
+    if len(arg) != 2:
+        sys.exit("Input and Target path are require.")
+    else:
+        main(arg)
